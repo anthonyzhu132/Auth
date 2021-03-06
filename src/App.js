d44bb8e95596180e5afdccf1b2f0030ee3894e47
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-03-05 23:19:41 -0800

    added re-usable components into project from previous project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
   render() {
     <View>
+      <Header headerText="Authentication" />
       <Text> An App</Text>
     </View>
   };
