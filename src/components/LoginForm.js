fbd5a2c82f2b7b2aa6c763a7c4753d25bbdec906
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-03-08 10:33:23 -0800

    refactored and added text inputs in LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import Button from '../components/common/Button';
 import Card from '../components/common/Card';
 import CardItem from '../components/common/CardItem';
@@ -8,10 +8,17 @@ class LoginForm extends Component {
   render() {
     return (
       <Card>
-        <CardItem />
-        <CardItem />
         <CardItem>
+          <TextInput style={{ height: 20, width: 100 }}>
 
+          </TextInput>
+        </CardItem>
+        <CardItem>
+          <TextInput>
+            
+          </TextInput>
+        </CardItem>
+        <CardItem>
           <Button>
             Log in
           </Button>
