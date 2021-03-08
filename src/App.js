cc32fcbcdf37cf6c0bcaf292b0e6e346ad4db1c3
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-03-08 09:55:01 -0800

    added firebase configutation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
 
 class App extends Component {
+  componenetWillMount() {
+     firebase.initializeApp({
+      apiKey: "AIzaSyAJNBgaWqwOVYtxMsF7lE5wbAjh9xzo70U",
+      authDomain: "auth-ebd5b.firebaseapp.com",
+      projectId: "auth-ebd5b",
+      storageBucket: "auth-ebd5b.appspot.com",
+      messagingSenderId: "223253966324",
+      appId: "1:223253966324:web:9f7207787272156e155da1",
+      measurementId: "G-TT2BQKB7Z7"
+     });
+  }
+  
   render() {
     <View>
       <Header headerText="Authentication" />
