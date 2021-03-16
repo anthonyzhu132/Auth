cefc6bb8679f8255aad04dfc77052af604274d99
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-03-15 22:03:41 -0700

    added event listener to change state to view if user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Header from './components/common/Header';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     const firebaseCofig = {
       apiKey: "AIzaSyAJNBgaWqwOVYtxMsF7lE5wbAjh9xzo70U",
@@ -17,6 +19,14 @@ class App extends Component {
     }
 
      firebase.initializeApp(firebaseCofig);
+
+     firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      };
+     })
   }
 
   render() {
